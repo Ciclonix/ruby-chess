0e186a0c0292f92e47066e06e48d32bdbb39b437
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-09-30 15:45:16 +0200

    Update class Chess -> include module NotationInterpreter

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "board"
+require_relative "notation_interpreter"
 
 class Chess
+  include NotationInterpreter
+
   def newGame
     @board = Board.new
   end
