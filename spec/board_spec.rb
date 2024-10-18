3fdc5b3841d954bf975b5146b1c3bb9e2f383d2d
Ciclonix <72094161+Ciclonix@users.noreply.github.com>
2024-10-18 13:23:37 +0200

    Update tests for class Board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "../lib/board"
-
-# rubocop:disable Metrics/BlockLength
+require_relative "../lib/piece"
 
 describe Board do
   subject(:board) { described_class.new }
-  let(:piece) { { from: [4, 4], color: :black, takes?: false, moves: [] } }
+  let(:piece) { instance_double("Piece") }
+
 
   describe "#addMove" do
     context "when the row or col are invalid" do
@@ -22,79 +22,117 @@ describe Board do
         expect(board.addMove(col, row, piece)).to be(nil)
       end
     end
-  end
 
-  describe "#canTake?" do
     context "when the target square is free" do
-      it "returns [true, true]" do
+      before do
+        expect(piece).to receive(:possible_moves).and_return([[], []])
+      end
+
+      it "returns true" do
         row = 5
         col = 5
-        expect(board.canTake?(col, row, piece[:color])).to eql([true, true])
+        expect(board.addMove(col, row, piece)).to eql(true)
       end
     end
 
     context "when the target square color is the same" do
-      it "returns [false, false]" do
+      before do
+        expect(piece).to receive(:color).and_return(:black)
+      end
+
+      it "returns false" do
         row = 7
         col = 7
-        expect(board.canTake?(col, row, piece[:color])).to eql([false, false])
+        expect(board.addMove(col, row, piece)).to eql(false)
       end
     end
 
     context "when the target square color is different" do
-      it "returns [true, false]" do
+      before do
+        expect(piece).to receive(:color).and_return(:black)
+        expect(piece).to receive(:possible_moves).and_return([[], []])
+      end
+
+      it "returns false" do
         row = 0
         col = 0
-        expect(board.canTake?(col, row, piece[:color])).to eql([true, false])
+        expect(board.addMove(col, row, piece)).to eql(false)
       end
     end
   end
 
   describe "#possibleRookMoves" do
     context "when the rook is at the center of an already set board" do
+      before do
+        expect(piece).to receive(:position).exactly(16).times.and_return([4, 4])
+        expect(piece).to receive(:color).twice.and_return(:black)
+        expect(piece).to receive(:possible_moves).exactly(13).times.and_return([[], []])
+      end
+
       it "returns the correct moves" do
-        moves = [[4, 5], [4, 3], [4, 2], [4, 1], [5, 4], [6, 4], [7, 4], [3, 4], [2, 4], [1, 4], [0, 4]]
-        expect { board.possibleRookMoves(piece) }.to change { piece[:moves] }.to eql(moves)
+        moves = [[[4, 5], [4, 3], [4, 2], [5, 4], [6, 4], [7, 4], [3, 4], [2, 4], [1, 4], [0, 4]], [[4, 1]]]
+        expect { board.possibleRookMoves(piece) }.to change { piece.possible_moves }.to eql(moves)
       end
     end
   end
 
   describe "#possibleKnightMoves" do
     context "when the knight is at the center of an already set board" do
+      before do
+        expect(piece).to receive(:position).exactly(16).times.and_return([4, 4])
+        expect(piece).to receive(:color).twice.and_return(:black)
+        expect(piece).to receive(:possible_moves).exactly(8).times.and_return([[], []])
+      end
+
       it "returns the correct moves" do
-        moves = [[3, 2], [5, 2], [2, 3], [6, 5], [2, 5], [6, 3]]
-        expect { board.possibleKnightMoves(piece) }.to change { piece[:moves] }.to eql(moves)
+        moves = [[[3, 2], [5, 2], [2, 3], [6, 5], [2, 5], [6, 3]], []]
+        expect { board.possibleKnightMoves(piece) }.to change { piece.possible_moves }.to eql(moves)
       end
     end
   end
 
   describe "#possibleBishopMoves" do
     context "when the bishop is at the center of an already set board" do
+      before do
+        expect(piece).to receive(:position).exactly(20).times.and_return([4, 4])
+        expect(piece).to receive(:color).exactly(4).times.and_return(:black)
+        expect(piece).to receive(:possible_moves).exactly(10).times.and_return([[], []])
+      end
+
       it "returns the correct moves" do
-        moves = [[3, 5], [5, 5], [3, 3], [2, 2], [1, 1], [5, 3], [6, 2], [7, 1]]
-        expect { board.possibleBishopMoves(piece) }.to change { piece[:moves] }.to eql(moves)
+        moves = [[[3, 5], [5, 5], [3, 3], [2, 2], [5, 3], [6, 2]], [[1, 1], [7, 1]]]
+        expect { board.possibleBishopMoves(piece) }.to change { piece.possible_moves }.to eql(moves)
       end
     end
   end
 
   describe "#possibleQueenMoves" do
     context "when the queen is at the center of an already set board" do
+      before do
+        expect(piece).to receive(:position).exactly(36).times.and_return([4, 4])
+        expect(piece).to receive(:color).exactly(6).times.and_return(:black)
+        expect(piece).to receive(:possible_moves).exactly(21).times.and_return([[], []])
+      end
+
       it "returns the correct moves" do
-        moves = [[4, 5], [4, 3], [4, 2], [4, 1], [5, 4], [6, 4], [7, 4], [3, 4], [2, 4], [1, 4],
-                 [0, 4], [3, 5], [5, 5], [3, 3], [2, 2], [1, 1], [5, 3], [6, 2], [7, 1]]
-        expect { board.possibleQueenMoves(piece) }.to change { piece[:moves] }.to eql(moves)
+        moves = [[[4, 5], [4, 3], [4, 2], [5, 4], [6, 4], [7, 4], [3, 4], [2, 4], [1, 4], [0, 4],
+                  [3, 5], [5, 5], [3, 3], [2, 2], [5, 3], [6, 2]], [[4, 1], [1, 1], [7, 1]]]
+        expect { board.possibleQueenMoves(piece) }.to change { piece.possible_moves }.to eql(moves)
       end
     end
   end
 
   describe "#possibleKingMoves" do
     context "when the king is at the center of an already set board" do
+      before do
+        expect(piece).to receive(:position).exactly(16).times.and_return([4, 4])
+        expect(piece).to receive(:possible_moves).exactly(10).times.and_return([[], []])
+      end
+
       it "returns the correct moves" do
-        moves = [[5, 5], [5, 4], [5, 3], [4, 5], [4, 3], [3, 5], [3, 4], [3, 3]]
-        expect { board.possibleKingMoves(piece) }.to change { piece[:moves] }.to eql(moves)
+        moves = [[[5, 5], [5, 4], [5, 3], [4, 5], [4, 3], [3, 5], [3, 4], [3, 3]], []]
+        expect { board.possibleKingMoves(piece) }.to change { piece.possible_moves }.to eql(moves)
       end
     end
   end
 end
-
-# rubocop:enable Metrics/BlockLength
